93258460fe6ef784c796cf0abf9d76fdd91fe95d
RapRap51 <85567635+RapRap51@users.noreply.github.com>
2023-07-06 23:26:36 +0700

    Add files via upload

---
+++ storage/framework/views/1b5e9128e5806227b61e5170ac80b7b7.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('content'); ?>
+    <?php echo $__env->make('default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Rafi\SEM 6\ISC22L3 - PEMROGRAMAN FRAMEWORK\Project\Modull_8_latihan\resources\views/profile.blade.php ENDPATH**/ ?>
